1714c3299ee47729c9ad1ba07c7758c7fe1424b5
JosePachuri993 <joseelpropio214@gmail.com>
2023-12-15 17:59:07 -0400

    Clase que usa la pila lista para guardar los infijos y convertirlo a postfijo en pilas

--- src/negocio/Postfijo.java
+++ src/negocio/Postfijo.java
@@ -6,91 +6,85 @@ package negocio;
  */
 public class Postfijo {
 
-    public Postfijo() {
+    PilaLista pilaSalida;
+    PilaLista pilaOperadores;
 
+    public Postfijo() {
+        pilaSalida = new PilaLista();        //numeros de salida
+        pilaOperadores = new PilaLista();
     }
 
     public int Jerarquia(String signo) {
         if ("+".equals(signo) || "-".equals(signo)) {
-        return 1;
-    }
-    if ("*".equals(signo) || "/".equals(signo)) {
-        return 2;
-    }
-    if ("^".equals(signo)) {
-        return 3;
-    }
-    return 0;
+            return 1;
+        }
+        if ("*".equals(signo) || "/".equals(signo)) {
+            return 2;
+        }
+        if ("^".equals(signo)) {
+            return 3;
+        }
+        return 0;
     }
 
     public void InfijaPostija(String expresion) {
-        PilaLista pilaSalida = new PilaLista();        //numeros de salida
-        PilaLista pilaOperadores = new PilaLista();       //()+-*/
+        pilaSalida = new PilaLista();        //numeros de salida
+        pilaOperadores = new PilaLista();       //()+-*/
         String aux = "";
 
         for (char caracter : expresion.toCharArray()) {
             aux = Character.toString(caracter);
             
-            if (aux.matches("[a-zA-Z0-9]+")) {                
-                pilaSalida.push(caracter);
-                
-//======================================================================================================                  
-            } else if (caracter == '(') {                
-                pilaOperadores.push(caracter);
-                
-//======================================================================================================                  
-            
+            if (aux.matches("[a-zA-Z0-9]+")) {
+                pilaSalida.push(caracter);                  
+            } else if (caracter == '(') { //(2-(2+5)*4)               
+                pilaOperadores.push(caracter);                  
             } else if (caracter == ')') {
-                
-                while (!pilaOperadores.vacia() /*&& (pilaOperadores.peek().toString() != "(") && !pilaOperadores.vacia()*/) {
-                    String a = pilaOperadores.peek().toString();
+                while (!pilaOperadores.vacia() ) {                    
                     if ((!"(".equals(pilaOperadores.peek().toString()))) {
                         pilaSalida.push(pilaOperadores.pop());
-                    }else {
+                    } else {
                         pilaOperadores.pop();
                         break;
                     }
-                } 
-                
-//======================================================================================================                
+                }
             } else if (caracter == '^' || caracter == '*' || caracter == '/' || caracter == '+' || caracter == '-') {
-                String b;int jer,jer2;
-                if(!pilaOperadores.vacia())
-                     b=pilaOperadores.peek().toString();
                 
-                while (!pilaOperadores.vacia() &&                        
-                      (Jerarquia(pilaOperadores.peek().toString()) >= Jerarquia(aux))) {
-                    jer=Jerarquia(pilaOperadores.peek().toString());
-                    jer2=Jerarquia(aux);
-                    b=pilaOperadores.peek().toString();
-                    pilaSalida.push(pilaOperadores.pop());
+                if (!pilaOperadores.vacia()) //b=pilaOperadores.peek().toString();
+                {
+                    while (!pilaOperadores.vacia()
+                            && (Jerarquia(pilaOperadores.peek().toString()) >= Jerarquia(aux))) {
+                        pilaSalida.push(pilaOperadores.pop());
+                    }
                 }
                 pilaOperadores.push(caracter);
             }
         }
-        
-        
-        //-------------------------------//======================================================================================================-------------------------------------------------------  
+        //==========================================================================
+          
         while (!pilaOperadores.vacia()) {
             pilaSalida.push(pilaOperadores.pop());
         }
-        pilaOperadores=null;
-        
+        pilaOperadores = null;
+        //System.out.println(pilaSalida.toString());
+    }
 
-        
-
-        System.out.println(pilaSalida.toString());
-        //System.out.println("===============================");
-        //System.out.println(pilaOperadores.toString());
+    public String getPilaSalida() {
+        return pilaSalida.toString();
     }
 
     public static void main(String[] args) {
         Postfijo a = new Postfijo();
-        String expresion = "(3-5)-(7+2)";
+        String expresion = "(2-(2+5)*4) ";
         a.InfijaPostija(expresion);
+        System.out.println(a.getPilaSalida());
+        System.out.println("");
+        String expresion2 = "(2-(2+5)*4) ";
+        a.InfijaPostija(expresion2);
+        System.out.println(a.getPilaSalida());
         //char caracter='/';
         //String aux = Character.toString(caracter);
-        
+
         //System.out.println(a.Jerarquia(aux));
     }
 
